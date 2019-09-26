25eceed3d78db0d1d8b6f0d514f921f10f5a5918
PitchRE <pitch@redvelvet.pro>
2019-09-26 17:42:18 +0200

    mobile targeted optimizations

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -2,10 +2,9 @@
   <div>
     <div class="uk-container">
       <vk-tabs align="center" class="uk-margin-large-top">
-        <vk-tabs-item title="Kuchnie">
+        <vk-tabs-item data-aos="zoom-in-up" title="Kuchnie">
           <gallery :images="kuchnie" :index="index" @close="index = null"></gallery>
           <div
-            data-aos="zoom-in-up"
             class="image"
             v-for="(image, imageIndex) in kuchnie"
             :key="imageIndex"
@@ -15,10 +14,9 @@
 
           <!-- galery end kuchnie -->
         </vk-tabs-item>
-        <vk-tabs-item title="Pokoje">
+        <vk-tabs-item data-aos="zoom-in-up" title="Pokoje">
           <gallery :images="pokoje" :index="index" @close="index = null"></gallery>
           <div
-            data-aos="zoom-in-up"
             class="image"
             v-for="(image, imageIndex) in pokoje"
             :key="imageIndex"
@@ -26,10 +24,9 @@
             :style="{ backgroundImage: 'url(' + image + ')', width: '300px', height: '200px' }"
           ></div>
         </vk-tabs-item>
-        <vk-tabs-item title="Łazienki">
+        <vk-tabs-item data-aos="zoom-in-up" title="Łazienki">
           <gallery :images="lazienki" :index="index" @close="index = null"></gallery>
           <div
-            data-aos="zoom-in-up"
             class="image"
             v-for="(image, imageIndex) in lazienki"
             :key="imageIndex"
